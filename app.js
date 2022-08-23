f7196fe1fb81a65f9c242182358241163e6246fa
h2o-Noob <arindamraina7743@gmail.com>
2022-08-23 10:42:49 +0530

    c24

--- app.js
+++ app.js
@@ -35,10 +35,10 @@ app.use("/api", payment);
 
 app.use(errorMiddleware)
 
-app.use(express.static(path.join(__dirname, "./client/build")))
+app.use(express.static(path.join(__dirname, "client/build")))
 
 app.get("*", (req,res)=>{
-    res.sendFile(path.resolve(__dirname, "./client/build/index.html"))
+    res.sendFile(path.resolve(__dirname, "client/build/index.html"))
 })
 
 module.exports = app
